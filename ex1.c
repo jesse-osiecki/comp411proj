b8ab777c093526fc87463e8f9c176afb43de8065
Jesse John Osiecki <osiecki@cs.unc.edu>
2013-11-20 18:04:17 -0500

    tried a partial commit, on everything though. It worked, git is neat. Anyhow, I have dictionaries of string arrays working now.

--- ex1.c
+++ ex1.c
@@ -3,12 +3,13 @@
 #include <stdio.h>
 #include <string.h>
 char *strdp(char *); //headers
-#define HASHSIZE 100001
-
+#define HASHSIZE 32767
+#define CHAINSIZE 1000
 typedef struct nlist { /* table entry: */
-    struct nlist *next; /* next entry in chain */
-    char *name; /* defined name */
-    char *defn; /* replacement text */
+	struct nlist *next; /* next entry in chain */
+	char *name; /* defined name */
+	char **defn; /* replacement text THIS IS AN ARRAY IN MY IMPL */
+	int defn_size;
 } NList;
 
 static NList *hashtab[HASHSIZE]; /* pointer table */
@@ -16,58 +17,63 @@ static NList *hashtab[HASHSIZE]; /* pointer table */
 /* hash: form hash value for string s */
 unsigned hash(char *s)
 {
-    unsigned hashval;
-    for (hashval = 0; *s != '\0'; s++)
-      hashval = *s + 31 * hashval;
-    return hashval % HASHSIZE;
+	unsigned hashval;
+	for (hashval = 0; *s != '\0'; s++)
+	  hashval = *s + 31 * hashval;
+	return hashval % HASHSIZE;
 }
 
 /* lookup: look for s in hashtab */
 NList *lookup(char *s)
 {
-    NList *np;
-    for (np = hashtab[hash(s)]; np != NULL; np = np->next)
-        if (strcmp(s, np->name) == 0)
-          return np; /* found */
-    return NULL; /* not found */
+	NList *np;
+	for (np = hashtab[hash(s)]; np != NULL; np = np->next)
+		if (strcmp(s, np->name) == 0)
+		  return np; /* found */
+	return NULL; /* not found */
 }
 
 /* install: put (name, defn) in hashtab */
 NList *install(char *name, char *defn)
 {
-    NList *np;
-    unsigned hashval;
-    if ((np = lookup(name)) == NULL) { /* not found */
-        np = (struct nlist *) malloc(sizeof(*np));
-        if (np == NULL || (np->name = strdup(name)) == NULL)
-          return NULL;
-        hashval = hash(name);
-        np->next = hashtab[hashval];
-        hashtab[hashval] = np;
-    } else /* already there */
-        free((void *) np->defn); /*free previous defn */
-    if ((np->defn = strdup(defn)) == NULL)
-       return NULL;
-    return np;
+	NList *np;
+	unsigned hashval;
+	if ((np = lookup(name)) == NULL) { /* not found */
+		np = (struct nlist *) malloc(sizeof(*np));
+		if (np == NULL || (np->name = strdp(name)) == NULL)
+		  return NULL;
+		hashval = hash(name);
+		np->next = hashtab[hashval];
+		hashtab[hashval] = np;
+		np->defn = malloc(CHAINSIZE * sizeof(char*));
+		np->defn_size = 0;
+	}//else /* already there /////*/
+	 //   free((void *) np->defn); /*free previous defn */
+	if((np->defn[np->defn_size] = strdp(defn)) == NULL)//Input was null
+	   return NULL;
+	else
+		np->defn_size++; //adding string to list succeeded to add one to size
+	return np;
 }
 
 char *strdp(char *s) /* make a duplicate of s */
 {
-    char *p;
-    p = (char *) malloc(strlen(s)+1); /* +1 for ’\0’ */
-    if (p != NULL)
-       strcpy(p, s);
-    return p;
+	char *p;
+	p = (char *) malloc(strlen(s)+1); /* +1 for ’\0’ */
+	if (p != NULL)
+	   strcpy(p, s);
+	return p;
 }
 
 /////////////////////////Dictionary End
 
-#define WORDSLENGTH 100001
+
+#define WORDSLENGTH 32767
 #define MAX_LINE 1000
 main(){
 	char **words;
-	char ***words_ref = &words;
-	printf("Enter a corpus of text from which the random text should be generated:\n\n");
+	int i = 0;
+//	printf("Enter a corpus of text from which the random text should be generated:\n\n");
 	char delim[11] =  " ,.!?;:\"\n";
 	words = malloc(WORDSLENGTH * sizeof(char*));//allocate space for huge array of words
 
@@ -78,19 +84,26 @@ main(){
 		char *pch = (char *) malloc(MAX_LINE);
 		pch = strtok(inpLine, delim); //returns begining of tokens
 		while(pch != NULL){ //put all new tokenized values into the words array
-			*words = (char *) malloc((strlen(pch)+1));
-			*words = pch;
-			words++;
-		       	pch = strtok(NULL, delim);//I find this weird, however it causes pch to return the next token succesively
+			words[i] = (char *) malloc((strlen(pch)+1));
+			words[i] = pch;
+			i++;
+		    pch = strtok(NULL, delim);//I find this weird, however it causes pch to return the next token succesively
 		}
 	}
-	*words = '\0';
-	words = *words_ref;
-
-	int i=0;
-	while(i<3){
-		printf("%s", *words);
-		words++;i++;
+	int array_size = i;
+	/*i=0;
+	while(i<array_size){
+		printf("%s\n", words[i]);
+		i++;
+	}
+	printf("done1");*/
+	for( i=0; i < array_size-1; i++){
+		install(words[i], words[i+1]);
+	}
+	for( i=0; i < 10; i++){
+		NList *np = lookup(words[i]);
+		if(np == NULL){ break;}
+		printf("%s%s\n", np->name, np->defn[0]);
 	}
 	return 0;
 }
